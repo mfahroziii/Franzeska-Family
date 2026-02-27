74540f2162b1eb06ef3b1a9c06497ba25f875f1d
mfahroziii <mfahrozi30@gmail.com>
2026-02-27 07:16:46 -0600

    qbx_core: change spawn & disable optin

--- resources/[qbx]/qbx_core/config/shared.lua
+++ resources/[qbx]/qbx_core/config/shared.lua
@@ -1,6 +1,6 @@
 return {
     serverName = 'Server',
-    defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
+    defaultSpawn = vec4(-1037.53, -2737.48, 12.78, 331.17),
     notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
     starterItems = { -- Character starting items
